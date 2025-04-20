5ed144fdccd39be7ee93764111677b17ea9af0fb
pazoit <bitsindapatrick@gamil.com>
2025-04-20 17:43:06 +0200

    chat app

--- script.js
+++ script.js
@@ -8,19 +8,23 @@ function addTask(){
     } else {
         const chat = document.createElement("p");
         chat.style.cssText = `
-    background-color: hsl(294, 28%, 87%);
-    color: hsl(276, 55%, 52%);
+    background-color:hsl(0, 0%, 100%);
+    color: hsl(271, 15%, 43%);
     font-size: 12px;
-    width: 70%;
+    width: 60%;
     height: 3rem;
     border-top-left-radius: 10px;
     border-top-right-radius: 10px;
-    border-bottom-right-radius: 10px;
-    border-bottom-left-radius: 5px;
-    padding: 8px;
+    border-bottom-right-radius: 5px;
+    border-bottom-left-radius: 10px;
+    padding: 10px;
+    margin-left:90px;
+
 `;
         chat.textContent = inputBox.value;
         listContainer.appendChild(chat);
+
+        listContainer.scrollTop = listContainer.scrollHeight;
     }
     inputBox.value = "";
     saveData();
